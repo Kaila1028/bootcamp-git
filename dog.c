4e6768847304ca44bd093b7337d85f587b7bf07d
Gonzalez <klgonzalez191@gd1-cset-19-07.tsct.us>
2019-09-17 12:44:01 -0400

    merged my dogs

--- dog.c
+++ dog.c
@@ -1,15 +1,16 @@
-puppy
-canine
-wolf
-bark
-bow wow
-pikachu
-charmander
-squirtle
-bulbasaur
-charmelon
-charizard
-ivysaur
-venasaur
-wartortle
-blastoise
+1 puppy
+2 canine
+3 wolf
+4 bark
+5 bow wow
+6 pikachu
+7 charmander
+8 squirtle
+9 bulbasaur
+10 charmelon
+11 charizard
+12 ivysaur
+13 venasaur
+14 wartortle
+15 blastoise
+
